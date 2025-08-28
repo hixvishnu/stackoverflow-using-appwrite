1986bba886c411c4bd137e40b04746cf8aab250e
hixvishnu <hixvishnu@gmail.com>
2025-08-28 18:36:30 +0530

    add the ui components

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,8 @@
 "use client"
+
+import { BackgroundBeams } from "@/components/ui/background-beams"
 import { useAuthStore } from "@/store/Auth"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import React from "react"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -18,8 +20,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   }
 
   return (
-    <div className="">
-      <div className="">{children}</div>
+    <div className="relative flex min-h-screen flex-col items-center justify-center py-12">
+      <BackgroundBeams />
+      <div className="relative">{children}</div>
     </div>
   )
 }
